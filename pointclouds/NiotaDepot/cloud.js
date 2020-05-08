951e65256358fa9e029e4a8d26cdbef700978cfd
DSTN4612 <DroneSurveyTN@gmail.com>
2020-05-08 07:09:50 -0400

    Initial commit

---
+++ pointclouds/NiotaDepot/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 6573693,
+    "boundingBox": {
+        "lx": 722446.94,
+        "ly": 3932864.63,
+        "lz": 210.29,
+        "ux": 722603.5100000003,
+        "uy": 3933021.2,
+        "uz": 366.860000000298
+    },
+    "tightBoundingBox": {
+        "lx": 722446.9400000001,
+        "ly": 3932864.63,
+        "lz": 210.29,
+        "ux": 722599.8200000001,
+        "uy": 3933021.2,
+        "uz": 229.49
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 1.3559359312057496,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
